15bcdfbb5585fe2e492198c008112a1d3c7bb168
Safique Ahmed Faruque <69505988+s-a-faruque@users.noreply.github.com>
2026-08-31 22:00:48 +0000

    error on typescript

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface Tally {
+  openPopup: (formId: string) => void;
+}
+
+declare global {
+  interface Window {
+    Tally?: Tally;
+  }
+}
+
+export {};
